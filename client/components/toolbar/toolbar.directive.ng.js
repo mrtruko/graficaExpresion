f220551eab9616a0036f13dc231c8f0b85ff4a20
Nicolas Gonzalez <GabrielMery@MacBook-Pro-de-erika.local>
2015-08-24 19:13:46 -0300

    Inicio

---
+++ client/components/toolbar/toolbar.directive.ng.js
@@ -0,0 +1,10 @@
+'use strict'
+
+angular.module('graficaExpresionApp')
+.directive('toolbar', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: 'client/components/toolbar/toolbar.view.ng.html',
+    replace: true
+  };
+});
---
+++ client/lib/app.ng.js
@@ -0,0 +1,16 @@
+angular.module('graficaExpresionApp', [
+  'angular-meteor',
+  'ui.router',
+  'ui.bootstrap',
+  'angularUtils.directives.dirPagination'
+]);
+
+onReady = function() {
+  angular.bootstrap(document, ['graficaExpresionApp']);
+};
+  
+if(Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
